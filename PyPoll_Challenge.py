4c2141d2d2a2c6247def47c52b9cad483fe03fda
Yan Kin Luong <yan.kin.luong@me.com>
2021-03-24 14:00:48 -0500

    pypoll challenged assignment

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -68,7 +68,6 @@ with open(file_to_load) as election_data:
 
         # 4a: Write an if statement that checks that the
         # county does not match any existing county in the county list.
-
         if county_name not in county_list:
 
             # 4b: Add the existing county to the list of counties.
@@ -79,7 +78,7 @@ with open(file_to_load) as election_data:
 
         # 5: Add a vote to that county's vote count.
         county_votes[county_name] +=1
-    #print[county_votes]
+    
 
 
 # Save the results to our text file.
@@ -93,21 +92,23 @@ with open(file_to_save, "w") as txt_file:
         f"-------------------------\n\n"
         f"County Votes:\n")
     print(election_results, end="")
-    print(county_votes)
+    #print(county_votes)
 
     txt_file.write(election_results)
 
     # 6a: Write a for loop to get the county from the county dictionary.
-
+    for county_name in county_votes:
         # 6b: Retrieve the county vote count.
-
+        votes_by_county = county_votes.get(county_name)
         # 6c: Calculate the percentage of votes for the county.
-
+        county_percentage = float(votes_by_county)/float(total_votes)*100
 
          # 6d: Print the county results to the terminal.
-
+        county_results = (f"{county_name}: {county_percentage:.1f}% ({votes_by_county:,})")
+        print(county_results)
+        
          # 6e: Save the county votes to a text file.
-
+        txt_file.write(county_results)
          # 6f: Write an if statement to determine the winning county and get its vote count.
 
 
